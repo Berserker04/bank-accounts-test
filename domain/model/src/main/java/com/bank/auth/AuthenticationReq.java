396451303ca0afff5b18124be9f0d6a4f356c54a
Berserker04 <xcarlosdesarrolladorx@gmail.com>
2023-06-26 07:10:42 -0500

    config security jwt

---
+++ domain/model/src/main/java/com/bank/auth/AuthenticationReq.java
@@ -0,0 +1,22 @@
+package com.bank.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class AuthenticationReq implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String usuario;
+
+    private String clave;
+
+    public AuthenticationReq(String usuario, String clave) {
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+}
--- infrastructure/entry-points/reactive-web/src/main/java/com/bank/client/ClientController.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/bank/client/ClientController.java
@@ -1,24 +1,41 @@
 package com.bank.client;
 
+import com.bank.auth.DemoRest;
 import com.bank.client.services.ClientService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/clients")
 public class ClientController {
+    private final ClientMapper mapper;
     private final ClientService clientService;
+    private static final Logger logger = LoggerFactory.getLogger(DemoRest.class);
 
     @PostMapping()
-    public Mono<Client> createClient(@RequestBody Client client) {
-        return clientService.createClient(client);
+    public ResponseEntity<?> createClient(@RequestBody Client client) {
+        SecurityContextHolder.getContext().getAuthentication();
+
+        return ResponseEntity.ok(clientService.createClient(client));
     }
 
     @GetMapping("/{id}")
-    public Mono<Client> getClientById(@PathVariable Long id) {
-        return clientService.getClientById(id);
+    public ResponseEntity<?> getClientById(@PathVariable Long id) {
+        SecurityContextHolder.getContext().getAuthentication();
+
+        Map<String, ClientData> mensaje = new HashMap<>();
+        mensaje.put("body", mapper.toEntityData(clientService.getClientById(id).block()).block());
+
+        return ResponseEntity.ok(mensaje);
     }
 
     @PutMapping()
